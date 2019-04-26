b813ef031baaf2e336574270adeb6ae39aa3e64d
KadirAydemir <37485465+KadirAydemir@users.noreply.github.com>
2019-04-26 21:30:39 +0300

    Date added

--- Unknowns/src/main/java/com/unknowns/controller/MyController.java
+++ Unknowns/src/main/java/com/unknowns/controller/MyController.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.unknowns.hibernate.util.HibernateUtil;
 import com.unknowns.hibernate.util.Queries;
 
 
--- Unknowns/src/main/java/com/unknowns/hibernate/entity/Knowledge.java
+++ Unknowns/src/main/java/com/unknowns/hibernate/entity/Knowledge.java
@@ -1,5 +1,7 @@
 package com.unknowns.hibernate.entity;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,32 +15,61 @@ public class Knowledge {
 	
 	String kname;
 	int level;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getKname() {
-		return kname;
-	}
-	public void setKname(String kname) {
-		this.kname = kname;
-	}
-	public int getLevel() {
-		return level;
-	}
-	public void setLevel(int level) {
-		this.level = level;
-	}
-	public Knowledge(String kname, int level) {
-		super();
-		this.kname = kname;
-		this.level = level;
-	}
+	Date date;
+	
+	
 	public Knowledge() {
 		super();
 	}
+
+
+	public Knowledge(String kname, int level, Date date) {
+		super();
+		this.kname = kname;
+		this.level = level;
+		this.date = date;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getKname() {
+		return kname;
+	}
+
+
+	public void setKname(String kname) {
+		this.kname = kname;
+	}
+
+
+	public int getLevel() {
+		return level;
+	}
+
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
 	
 	
 }
