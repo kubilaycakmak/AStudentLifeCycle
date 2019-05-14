27b0efc10911095773285c7d53f378cd2e957b35
Kadir Aydemir <kadiraydemir19907@gmail.com>
2019-05-15 00:59:07 +0300

    Company Fix

--- Unknowns/src/main/java/com/unknowns/hibernate/entity/Items.java
+++ Unknowns/src/main/java/com/unknowns/hibernate/entity/Items.java
@@ -1,11 +1,14 @@
 package com.unknowns.hibernate.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Items {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int id;
 	
 	int count1;
